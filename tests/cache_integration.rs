ed08c1ede61bf2239c90d325bf79f2df614d789f
githvinayak <vinayak192002@gmail.com>
2026-06-21 13:37:58 +0530

    test: add integration tests for realistic cache flows

--- tests/cache_integration.rs
+++ tests/cache_integration.rs
@@ -1,10 +1,9 @@
 use lru_core::BasicCache;
-
+use lru_core::Cache;
 
 #[cfg(test)]
 mod tests{
-    use lru_core::Cache;
-    use super::BasicCache;
+    use super::*;
     #[test]
     fn test_integration_flow(){
         let fetched_value:i32 = 5;
@@ -12,5 +11,28 @@ mod tests{
         cache.put(String::from("test"), fetched_value).unwrap();
         let cached_value  =  cache.get(&"test".to_string());
         assert_eq!(cached_value,Some(&fetched_value));
+    }
+    #[test]
+    fn test_multiple_deletion(){
+        let mut cache:BasicCache<String,i32>  = BasicCache::new();
+        cache.put(String::from("one"), 1).unwrap();
+        cache.put(String::from("two"), 2).unwrap();
+        cache.put(String::from("three"), 3).unwrap();
+        cache.put(String::from("four"), 4).unwrap();
+        cache.put(String::from("five"), 5).unwrap();
+        cache.remove(&"one".to_string());
+        cache.remove(&"two".to_string());
+        cache.remove(&"three".to_string());
+        cache.remove(&"four".to_string());
+        cache.remove(&"five".to_string());
+        assert!(cache.is_empty());
+    }
+
+    #[test]
+    fn test_overwrite_flow(){
+        let mut cache:BasicCache<String,i32>  = BasicCache::new();
+        cache.put(String::from("one"), 1).unwrap();
+        cache.put(String::from("one"), 5).unwrap();
+        assert_eq!(cache.get(&"one".to_string()),Some(&5));
     }
 }
