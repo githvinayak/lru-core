2a6b943caf51217d99028334a74a5f00ba4518b5
githvinayak <vinayak192002@gmail.com>
2026-07-08 17:22:27 +0530

    learning: introduce tokio spawn for concurrency

--- src/bin/async_demo.rs
+++ src/bin/async_demo.rs
@@ -1,9 +1,24 @@
-async fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
+use std::time::Duration;
+// async fn add(a: i32, b: i32) -> i32 {
+//     a + b
+// }
 
 #[tokio::main]
 async fn main() {
-    let future = add(2, 3).await;
-    println!("future created {:?}", future);
+    // let future = add(2, 3).await;
+    // println!("future created {:?}", future);
+
+    let mut handles = vec![];
+    for i in 1..4{
+        let task = tokio::spawn(async move {
+            tokio::time::sleep(Duration::from_millis(1000)).await;
+            println!("{}", i);
+        });
+        handles.push(task);
+    }
+
+    for handle in handles{
+        handle.await.unwrap();
+    }
+
 }
