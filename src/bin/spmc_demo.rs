02149e77ee883f640f09855317fab875350ff352
githvinayak <vinayak192002@gmail.com>
2026-07-16 16:48:06 +0530

    build: multi consumer worker pool with Arc Mutex receiver

---
+++ src/bin/spmc_demo.rs
@@ -0,0 +1,40 @@
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio::sync::mpsc;
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = mpsc::channel::<i32>(9);
+    let rx = Arc::new(Mutex::new(rx));
+    let rx1 = rx.clone();
+    let rx2 = rx.clone();
+    async fn consumer(id:i32,rx: Arc<Mutex<mpsc::Receiver<i32>>>){
+        while let Some(msg) = rx.lock().await.recv().await {
+            println!("worker id :{}, rec msg:{}",id, msg);
+        }
+    }
+    let task = tokio::spawn(async move{
+        tx.send(1).await;
+        tx.send(2).await;
+        tx.send(3).await;
+        tx.send(4).await;
+        tx.send(5).await;
+        tx.send(6).await;
+        tx.send(7).await;
+        tx.send(8).await;
+        tx.send(9).await;
+    });
+     let consumer_one = tokio::spawn(async move {
+        consumer(1,rx.clone()).await;
+    });
+    let consumer_two = tokio::spawn(async move {
+        consumer(2,rx1.clone()).await;
+    });
+    let consumer_three = tokio::spawn(async move {
+        consumer(3,rx2.clone()).await;
+    });
+    task.await.unwrap();
+    consumer_one.await.unwrap();
+    consumer_two.await.unwrap();
+    consumer_three.await.unwrap();
+}
