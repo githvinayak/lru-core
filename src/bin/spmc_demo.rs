d179a6d581f31161a150b0ad458283780058a2bd
githvinayak <vinayak192002@gmail.com>
2026-07-21 18:17:32 +0530

    build: graceful shutdown for async tasks

--- src/bin/spmc_demo.rs
+++ src/bin/spmc_demo.rs
@@ -8,32 +8,43 @@ async fn main() {
     let rx = Arc::new(Mutex::new(rx));
     let rx1 = rx.clone();
     let rx2 = rx.clone();
-    async fn consumer(id:i32,rx: Arc<Mutex<mpsc::Receiver<i32>>>){
-        while let Some(msg) = rx.lock().await.recv().await {
-            println!("worker id :{}, rec msg:{}",id, msg);
+    let (shutdown_tx1, shutdown_rx1) = mpsc::channel::<()>(1);
+    let (shutdown_tx2, shutdown_rx2) = mpsc::channel::<()>(1);
+    let (shutdown_tx3, shutdown_rx3) = mpsc::channel::<()>(1);
+    async fn consumer(id: i32, rx: Arc<Mutex<mpsc::Receiver<i32>>>,mut shutdown_rx: mpsc::Receiver<()>) {
+        loop {
+            let mut guard  = rx.lock().await;
+            tokio::select! {
+                Some(msg) = guard.recv()=> { println!("worker id :{}, rec msg:{}",id, msg);}
+             _ = shutdown_rx.recv() => { break; }  // shutdown signal received
+      }
         }
     }
-    let task = tokio::spawn(async move{
+
+    let task = tokio::spawn(async move {
         tx.send(1).await;
         tx.send(2).await;
         tx.send(3).await;
         tx.send(4).await;
-        tx.send(5).await;
+       tx.send(5).await;
         tx.send(6).await;
         tx.send(7).await;
         tx.send(8).await;
         tx.send(9).await;
     });
-     let consumer_one = tokio::spawn(async move {
-        consumer(1,rx.clone()).await;
+    let consumer_one = tokio::spawn(async move {
+        consumer(1, rx,shutdown_rx1).await;
     });
     let consumer_two = tokio::spawn(async move {
-        consumer(2,rx1.clone()).await;
+        consumer(2, rx1,shutdown_rx2).await;
     });
     let consumer_three = tokio::spawn(async move {
-        consumer(3,rx2.clone()).await;
+        consumer(3, rx2,shutdown_rx3).await;
     });
     task.await.unwrap();
+    shutdown_tx1.send(()).await.unwrap();
+    shutdown_tx2.send(()).await.unwrap();
+    shutdown_tx3.send(()).await.unwrap();
     consumer_one.await.unwrap();
     consumer_two.await.unwrap();
     consumer_three.await.unwrap();
