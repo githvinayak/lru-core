db358b6ef9a6ae9891b6c6628f9fc39133884e1f
githvinayak <vinayak192002@gmail.com>
2026-07-23 12:01:07 +0530

    build: graceful shutdown via channel close, multi consumer worker pool

--- src/bin/spmc_demo.rs
+++ src/bin/spmc_demo.rs
@@ -8,16 +8,13 @@ async fn main() {
     let rx = Arc::new(Mutex::new(rx));
     let rx1 = rx.clone();
     let rx2 = rx.clone();
-    let (shutdown_tx1, shutdown_rx1) = mpsc::channel::<()>(1);
-    let (shutdown_tx2, shutdown_rx2) = mpsc::channel::<()>(1);
-    let (shutdown_tx3, shutdown_rx3) = mpsc::channel::<()>(1);
-    async fn consumer(id: i32, rx: Arc<Mutex<mpsc::Receiver<i32>>>,mut shutdown_rx: mpsc::Receiver<()>) {
+    async fn consumer(id: i32, rx: Arc<Mutex<mpsc::Receiver<i32>>>) {
         loop {
-            let mut guard  = rx.lock().await;
-            tokio::select! {
-                Some(msg) = guard.recv()=> { println!("worker id :{}, rec msg:{}",id, msg);}
-             _ = shutdown_rx.recv() => { break; }  // shutdown signal received
-      }
+            let msg = rx.lock().await.recv().await;
+            match msg {
+                Some(msg) => println!("id {}: msg {}", id,msg),
+                None => break,
+            }
         }
     }
 
@@ -33,18 +30,15 @@ async fn main() {
         tx.send(9).await;
     });
     let consumer_one = tokio::spawn(async move {
-        consumer(1, rx,shutdown_rx1).await;
+        consumer(1, rx).await;
     });
     let consumer_two = tokio::spawn(async move {
-        consumer(2, rx1,shutdown_rx2).await;
+        consumer(2, rx1).await;
     });
     let consumer_three = tokio::spawn(async move {
-        consumer(3, rx2,shutdown_rx3).await;
+        consumer(3, rx2).await;
     });
     task.await.unwrap();
-    shutdown_tx1.send(()).await.unwrap();
-    shutdown_tx2.send(()).await.unwrap();
-    shutdown_tx3.send(()).await.unwrap();
     consumer_one.await.unwrap();
     consumer_two.await.unwrap();
     consumer_three.await.unwrap();
