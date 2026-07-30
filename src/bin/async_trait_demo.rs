32e4a629b794538ed16256c8a0640f09d8f2bd7f
githvinayak <vinayak192002@gmail.com>
2026-07-30 21:49:44 +0530

    build: multiple async worker implementations with dynamic dispatch

--- src/bin/async_trait_demo.rs
+++ src/bin/async_trait_demo.rs
@@ -1,18 +1,28 @@
 use std::time::Duration;
 use async_trait::async_trait;
+
 #[async_trait]
 trait AsyncWorker{
     async fn process(&self,task:i32);
 }
-
+#[derive(Debug)]
 struct PrintWorker;
-
+#[derive(Debug)]
+struct LogWorker;
 
 impl PrintWorker{
     pub fn new()->Self{
         PrintWorker{}
     }
 }
+
+impl LogWorker{
+    pub fn new()->Self{
+        LogWorker{}
+    }
+}
+
+
 #[async_trait]
 impl AsyncWorker for PrintWorker{
     async fn process(&self,task:i32){
@@ -21,9 +31,16 @@ impl AsyncWorker for PrintWorker{
     }
 }
 
+#[async_trait]
+impl AsyncWorker for LogWorker{
+    async fn process(&self,task:i32){
+        println!("logged: {task}");
+    }
+}
 #[tokio::main]
 async fn main() {
-
-    let worker = PrintWorker::new();
-    worker.process(5).await;
+    let workers : Vec<Box<dyn AsyncWorker>> = vec![Box::new(PrintWorker::new()),Box::new(LogWorker::new())];
+    for worker in workers{
+        worker.process(5).await;
+    }
 }
