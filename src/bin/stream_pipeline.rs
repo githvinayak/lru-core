6051a71808791c2a1094d87b72d87ec0ad8a86ec
githvinayak <vinayak192002@gmail.com>
2026-07-31 17:52:26 +0530

    experiment: observe backpressure under burst load with detailed timing

--- src/bin/stream_pipeline.rs
+++ src/bin/stream_pipeline.rs
@@ -1,6 +1,6 @@
 use tokio::sync::Mutex;
 use std::sync::Arc;
-use std::time::Duration;
+use std::time::{Instant,Duration};
 use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 
@@ -9,36 +9,41 @@ async fn main() {
     let (tx,mut rx) = mpsc::channel::<i32>(3);
     let rx = Arc::new(Mutex::new(rx));
     let rx1 = rx.clone();
-    async fn consumer(mut rx:Arc<Mutex<mpsc::Receiver<i32>>>) {
+    let start  = Arc::new(Mutex::new(Instant::now()));
+    let start1 = start.clone();
+    async fn consumer(id:i32,mut rx:Arc<Mutex<mpsc::Receiver<i32>>>,start:Arc<Mutex<Instant>>) {
         loop{
             let msg  = rx.lock().await.recv().await;
+            println!("worker {} received {:?} at {}ms, processing...",id, msg, start.lock().await.elapsed().as_millis());
+            tokio::time::sleep(Duration::from_millis(300)).await;
+            println!("worker {} finished {:?} at {}ms",id, msg, start.lock().await.elapsed().as_millis());
             match msg {
                 Some(msg) => {
                     println!(" msg {}",msg);
                 }
                 None => break
             }
-            tokio::time::sleep(Duration::from_millis(300)).await;
+
         }
     }
 
     let task = tokio::spawn(async move {
         let mut stream = tokio_stream::iter(vec![1, 2, 3,4,5,6,7,8,9,10]);
         while let Some(val) = stream.next().await{
-            let start = std::time::Instant::now();
+            let start = Instant::now();
             println!("sending {} at {:?}", val, start.elapsed());
             tx.send(val).await.unwrap();
-            println!("sent {} at {:?}", val, start.elapsed());
+            println!("sent {} at {}ms", val, start.elapsed().as_millis());
             tokio::time::sleep(Duration::from_millis(10)).await;
         }
     });
 
     let worker_one = tokio::spawn(async move {
-        consumer(rx).await;
+        consumer(1,rx,start).await;
     });
 
     let worker_two = tokio::spawn(async move {
-        consumer(rx1).await;
+        consumer(2,rx1,start1).await;
     });
 
     task.await.unwrap();
